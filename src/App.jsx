5d9ea0abccd6c17c39c49dae81d1b01ef25db92b
Rahul Kumar <rahul@Rahuls-MacBook-Air.local>
2024-12-20 12:52:28 -0800

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import { SideBar } from './components/SideBar'
+import Player from './components/Player'
+import Display from './components/Display'
 
 const App = () => {
   return (
     <div className='h-screen bg-black'>
       <div className='h-[90%] flex'>
         <SideBar/>
+        <Display/>
+      </div>
+      <div>
+        <Player/>
       </div>
     </div>
   )
