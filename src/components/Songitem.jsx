f502f012f508cf6999f4fc81a34a23b66f031aae
Rahul Kumar <rahul@rahuls-air.mynetworksettings.com>
2024-12-22 12:53:04 -0800

    changes

--- src/components/Songitem.jsx
+++ src/components/Songitem.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { PlayerContext } from '../context/PlayerContext'
 
 const Songitem = ({name, image, desc, id}) => {
+
+  const {playwithId} = useContext(PlayerContext);
   return (
-    <div className='min-w-[180px] p-2 px-3 rounded cursor-pointer hover:bg-[$ffffff26]'>
+    <div onClick={()=> playwithId(id)}className='min-w-[180px] p-2 px-3 rounded cursor-pointer hover:bg-[$ffffff26]'>
         <img className='rounded'src={image} alt="" />
         <p className='font-bold mt-2 mb-2'></p>
         <p className='text-slate-200 text-sm'>{desc}</p>
